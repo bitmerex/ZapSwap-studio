a5f12712ae160c96be6b0bdf553ae1dd24dd75f6
William Poulin <will@zapper.fi>
2023-02-24 13:57:27 -0500

    fix(studio): Fetch collateral using Arcade's subgraph

--- src/apps/lyra-avalon/lyra-avalon.module.ts
+++ src/apps/lyra-avalon/lyra-avalon.module.ts
@@ -3,8 +3,8 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { LyraAvalonContractFactory } from './contracts';
-import { EthereumLyraAvalonStkLyraTokenFetcher } from './ethereum/lyra-avalon.stk-lyra.token-fetcher';
 import { EthereumLyraAvalonStakingContractPositionFetcher } from './ethereum/lyra-avalon.staking.contract-position-fetcher';
+import { EthereumLyraAvalonStkLyraTokenFetcher } from './ethereum/lyra-avalon.stk-lyra.token-fetcher';
 import { OptimismLyraAvalonOptionsContractPositionFetcher } from './optimism/lyra-avalon.options.contract-position-fetcher';
 import { OptimismLyraAvalonPoolTokenFetcher } from './optimism/lyra-avalon.pool.token-fetcher';
 import { OptimismLyraAvalonStakingContractPositionFetcher } from './optimism/lyra-avalon.staking.contract-position-fetcher';
@@ -23,4 +23,4 @@ import { OptimismLyraAvalonStkLyraTokenFetcher } from './optimism/lyra-avalon.st
     EthereumLyraAvalonStakingContractPositionFetcher,
   ],
 })
-export class LyraAvalonAppModule extends AbstractApp() { }
+export class LyraAvalonAppModule extends AbstractApp() {}
